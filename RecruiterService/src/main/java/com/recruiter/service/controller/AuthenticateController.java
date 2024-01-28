42e05d47b2810904a8bcd194e9beb43daca48b74
Neelesh kumar <neeleshkumarm98@gmail.com>
2024-01-28 20:34:04 +0530

    resolve current-user api

--- RecruiterService/src/main/java/com/recruiter/service/controller/AuthenticateController.java
+++ RecruiterService/src/main/java/com/recruiter/service/controller/AuthenticateController.java
@@ -9,11 +9,11 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.recruiter.service.config.JwtUtils;
 import com.recruiter.service.model.JwtRequest;
@@ -21,7 +21,7 @@ import com.recruiter.service.model.JwtResponse;
 import com.recruiter.service.model.Recruiter;
 import com.recruiter.service.serviceImpl.UserDetailsSecurityServiceImpl;
 
-@Controller
+@RestController
 @CrossOrigin("*")
 public class AuthenticateController {
 
